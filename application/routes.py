eb18922fd4ca161e931181078f5570dbb5b514e0
michelle548129 <michelle548129@gmail.com>
2022-03-10 18:45:39 +0000

    finished adding to list function

--- application/routes.py
+++ application/routes.py
@@ -7,6 +7,20 @@ from application.forms import AddItem, UpdateItem, ChooseItem
 @app.route('/home', methods=["GET", "POST"])
 @app.route('/', methods=["GET", "POST"])
 def home():
+   if request.form:
+      print(request.form)
+   return render_template('home.html')
+
+
+@app.route('/basket', methods=["GET", "POST"])
+def basket():
+   if request.form:
+      print(request.form)
+   return render_template('basket.html')
+
+
+@app.route('/admin_page', methods=["GET", "POST"])
+def admin_page():
    form = AddItem()
    if request.method == 'POST':
       item_name = form.item_name.data
@@ -15,21 +29,8 @@ def home():
       newItem = item(item_name = item_name, price=price, description = description)
       db.session.add(newItem)
       db.session.commit()
-   return render_template('home.html')
+   return render_template('admin_page.html')
 
 
-@app.route('/shop', methods=["GET", "POST"])
-def shop():
-   if request.form:
-      item = Item(item_name=request.form.get("Item Name"))
-      db.session.add(item)
-      db.session.commit()
-   return render_template('shop.html')
-
-@app.route('/basket', methods=["GET", "POST"])
-def basket():
-   if request.form:
-      print(request.form)
-   return render_template('basket.html')
 
 
